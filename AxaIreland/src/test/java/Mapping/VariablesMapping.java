7a00351868168c3d45cda62a30b28aa8c43e29cb
Dominik <dominik@com>
2022-11-26 21:36:11 +0100

    VariablesMapping

---
+++ AxaIreland/src/test/java/Mapping/VariablesMapping.java
@@ -0,0 +1,48 @@
+package Mapping;
+
+public class VariablesMapping {
+
+	public static String Registration;
+	public static String BusinessPurpouse;
+	public static String CommunityPurpouse;
+	public static String TypeOfBusiness;
+	public static String KM;
+	public static String Male;
+	public static String Name;
+	public static String Surname;
+	public static double Day;
+	public static double Month;
+	public static double Year;
+	public static String email;
+	public static double Phone;
+	public static String Employment;
+	public static String Occupation;
+	public static String Adress;
+	public static String TypeofHouseHold;
+	public static String Licence;
+	public static String EssentialDriving;
+	public static String LicenceTime;
+	public static String PenaltyPoints;
+	public static double HowManyPenalty;
+	public static String Reason;
+	public static String RecentInsurance;
+	public static String NCD;
+	public static String Discount;
+	public static String Breakdown;
+	public static String OpenDriving;
+	public static String ProtectedNoClaim;
+	public static String InsuranceExpiryQuestion;
+	public static String ExpiryData;
+	public static String IsPolicyRefused;
+	public static String EstimatedCarValue;
+	public static String CarValue;
+	public static String IsCarModified;
+	public static String DidYouPurchaseACar;
+	public static String IsRegisterToYourSpouse;
+	public static String MainDriver;
+	public static String ClaimDeclined;
+	public static String ProsecutionPending;
+	public static String MedicalCondition;
+	
+	
+}
